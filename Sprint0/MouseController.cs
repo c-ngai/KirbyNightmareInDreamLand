3a2768e5105f432fa833632ba7f907dc35dbc18d
Carman Ngai <MY GITHUB EMAIL>
2024-08-29 19:14:24 -0400

    Mouse Controller WIP.

--- Sprint0/MouseController.cs
+++ Sprint0/MouseController.cs
@@ -1,32 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 
 namespace Sprint0;
 public class MouseController : IController
 {
-    private Dictionary<ButtonState, ICommand> controllerMappings;
+    private Rectangle firstQuadrant;
+    private Rectangle secondQuadrant;
+    private Rectangle thirdQuadrant;
+    private Rectangle fourthQuadrant;
 
-    public MouseController()
+    private Dictionary<MouseControllerState, ICommand> controllerMappings;     
+
+    public MouseController(int width, int height)
     {
-        controllerMappings = new Dictionary<ButtonState, ICommand>();
+        // creates starting quadrant point 
+        Point origin = new Point(0, 0);
+        Point topBorderMidPoint = new Point(width / 2, 0);
+        Point leftBorderMidPoint = new Point(0, height / 2);
+        Point center = new Point(width / 2, height / 2);
+
+        // creates size of quadrant
+        Point size = new Point(width / 2, height / 2);
+
+        // creates quadrants
+        firstQuadrant = new Rectangle(origin, size);
+        secondQuadrant = new Rectangle(topBorderMidPoint, size);
+        thirdQuadrant = new Rectangle(leftBorderMidPoint, size);
+        fourthQuadrant = new Rectangle(center, size);
+
+        controllerMappings = new Dictionary<MouseControllerState, ICommand>();
+        
     }
 
-    public void RegisterCommand(ButtonState button, ICommand command)
+    public void RegisterCommand(MouseControllerState state, ICommand command)
     {
-        controllerMappings.Add(button, command);
+        controllerMappings.Add(state, command);
     }
 
     public void Update()
     {
-        MouseState state = Mouse.GetState();
-
-        // Create Commands and Register Them
-
-        foreach (KeyValuePair<ButtonState, ICommand> button in controllerMappings)
-        {
-            controllerMappings[button.Key].Execute();
-        }
+        MouseState mouse = GetState();
     }
 }
---
+++ Sprint0/MouseControllerState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sprint0
+{
+    public class MouseControllerState
+    {
+
+        private bool leftClick { get; set; }
+        private bool rightClick { get; set; }
+
+        private int xPosition { get; set; }
+        private int yPosition { get; set;  }
+
+        public MouseControllerState()
+        {
+            leftClick = false;
+            rightClick = false;
+
+            xPosition = 0;
+            yPosition = 0;
+
+        }
+    }
+}
