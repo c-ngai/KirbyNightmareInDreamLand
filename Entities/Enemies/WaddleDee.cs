1330454ed30ffe358b8cf71e646070ae523695d9
gbarnholtz <dbcgwyn@gmail.com>
2024-09-15 17:32:27 -0400

    beginning enemies

---
+++ Entities/Enemies/WaddleDee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using System;
+using Microsoft.Xna.Framework;
+
+namespace MasterGame.Entities.Enemies
+{
+    public class WaddleDee : IEnemy
+    {
+        private Vector2 position;
+        private int health;
+        private int damage;
+        //private ISprite sprite;
+        public void takeDamage()
+        {
+             
+        }
+        public void attack()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
+        public void ChangeDirection()
+        {
+
+        }
+    }
+
+}
