250972a6fed553ecc8934988efbfbc5d9459451f
gbarnholtz <dbcgwyn@gmail.com>
2024-09-21 18:02:52 -0400

    Waddle Dee adjusted to match Player class (thank u Lina)

--- Game1.cs
+++ Game1.cs
@@ -28,6 +28,9 @@ namespace MasterGame
 
         public IPlayer kirby;
         // get kirby 
+
+        public IEnemy waddledeeTest;
+        //get waddle dee
         
         public Game1()
         {
@@ -96,6 +99,9 @@ namespace MasterGame
             SpriteFactory.Instance.LoadAllSpriteAnimations();
             // Create a test sprite (TEMPORARY)
             kirby.PlayerSprite = SpriteFactory.Instance.createSprite("kirby_normal_standing_right");
+            //waddledeeTest.EnemySprite = SpriteFactory.Instance.createSprite("kirby_normal_standing_right");
+
+            waddledeeTest = new WaddleDee(new Vector2(150, 100));
 
             TestSprite1 = SpriteFactory.Instance.createSprite("kirby_normal_walking_right");
             TestSprite2 = SpriteFactory.Instance.createSprite("tile_waterfall");
@@ -119,7 +125,8 @@ namespace MasterGame
 
             TestSprite1.Update();
             TestSprite2.Update();
-            kirby.Update(); 
+            kirby.Update();
+            waddledeeTest.Update();
             //TestSprite.Update();
         }
 
@@ -156,6 +163,7 @@ namespace MasterGame
             //TestSprite2.Draw(new Vector2((int)(Mouse.GetState().X/scale), (int)(Mouse.GetState().Y/scale)));
 
             kirby.Draw();
+            waddledeeTest.Draw();
             // End spriteBatch
             spriteBatch.End();
 
