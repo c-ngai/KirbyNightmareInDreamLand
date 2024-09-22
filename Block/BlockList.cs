95ba7ea4a21659e7b4518af89519f9458f462b1a
goose0 <107486126+goose0@users.noreply.github.com>
2024-09-21 21:40:18 -0400

    Update BlockList.cs
    trying to fix erroneous commits

--- Block/BlockList.cs
+++ Block/BlockList.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MasterGame.Block
 { 
-
     public class BlockList
     {
 
@@ -14,68 +13,6 @@ namespace MasterGame.Block
         int lastBlock;
 
         public BlockList(List<Texture2D> myBlocks)
-        {
-        }
-
-        // Singleton instance method. Allows other files to access this classes variables. 
-        public static BlockList Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new BlockList();
-                }
-                return instance;
-            }
-        }
-
-        // Singleton constructor must be empty, so this does constructor stuff.
-        public void setBlockList(List<Sprite> myBlocks)
-        {
-        }
-            if (currentBlock == firstBlock)
-            {
-                currentBlock = lastBlock;
-            }
-            else
-            {
-                currentBlock--;
-            }
-
-        // Singleton instance method. Allows other files to access this classes variables. 
-        public static BlockList Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new BlockList();
-                }
-                return instance;
-            }
-        }
-
-        // Singleton constructor must be empty, so this does constructor stuff.
-        public void setBlockList(List<Sprite> myBlocks)
-        {
-        }
-
-        // Singleton instance method. Allows other files to access this classes variables. 
-        public static BlockList Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
-                    instance = new BlockList();
-                }
-                return instance;
-            }
-        }
-
-        // Singleton constructor must be empty, so this does constructor stuff.
-        public void setBlockList(List<Sprite> myBlocks)
         {
             blockList = myBlocks;
             currentBlock = firstBlock;
@@ -96,24 +33,14 @@ namespace MasterGame.Block
 
         public void viewPrevious()
         {
-            if (currentBlock > firstBlock)
-            {
-                currentBlock--;
-            }
-            else
+            if (currentBlock == firstBlock)
             {
                 currentBlock = lastBlock;
             }
-        }
-
-        // Draws the current block. 
-        public void Draw(Vector2 location)
-        {
-            if (currentBlock >= 0 && currentBlock < blockList.Count)
+            else
             {
-                blockList[currentBlock].Draw(location);
+                currentBlock--;
             }
-        }
 
         }
 
