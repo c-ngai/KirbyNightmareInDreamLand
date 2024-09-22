7c4d48ecdac7e0e321c519e087a3fad71a9b4821
goose0 <107486126+goose0@users.noreply.github.com>
2024-09-21 22:36:19 -0400

    Update DisplayBlocks.cs

--- Block/DisplayBlocks.cs
+++ Block/DisplayBlocks.cs
@@ -8,20 +8,20 @@ namespace MasterGame.Block
 	public class DisplayBlocks
 	{
 		public DisplayBlocks()
-        {
-        }
-
-        public void Draw(SpriteBatch spriteBatch, Vector2 location)
-        {
-
-
-            //spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
-        }
-        public void Update()
-        {
-            // do nothing 
+        	{
 		}
 
-	}
+		public void Draw(SpriteBatch spriteBatch, Vector2 location)
+        	{
+
+
+            		//spriteBatch.Draw(texture, destinationRectangle, sourceRectangle, Color.White);
+        	}
+        	public void Update()
+        	{
+           	 	// do nothing 
+		}
+
+        }
 }
 
