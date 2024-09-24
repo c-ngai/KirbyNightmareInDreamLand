fa2a05d0556b5b35a91085769eda8ced7d0fc4b8
gbarnholtz <dbcgwyn@gmail.com>
2024-09-24 17:49:57 -0400

    temp hothead update to attempt projectiles

--- Entities/Enemies/Hothead.cs
+++ Entities/Enemies/Hothead.cs
@@ -3,7 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace MasterGame
 {
-    public class WaddleDoo : IEnemy
+    public class Hothead : IEnemy
     {
         private Vector2 position;
         private int health;
@@ -17,15 +17,17 @@ namespace MasterGame
         private int walkFrames = 180;  //3 sec (if 60fps)
         private int stopFrames = 60;  //2 sec
         private int attackFrames = 100; //1 sec
+        private int shootFrames = 100; //1 sec
+        private IProjectile fireball;
 
-        public WaddleDoo(Vector2 startPosition)
+        public Hothead(Vector2 startPosition)
         {
             position = startPosition;
             health = 100;
             isDead = false;
             stateMachine = new EnemyStateMachine(EnemyType.WaddleDoo);
-            //stateMachine.ChangePose(EnemyPose.Walking);
-           enemySprite = SpriteFactory.Instance.createSprite("waddledoo_walking_right");
+            //stateMachine = new EnemyStateMachine(EnemyType.Hothead);
+            stateMachine.ChangePose(EnemyPose.Walking);
         }
 
         public Vector2 Position
@@ -61,16 +63,19 @@ namespace MasterGame
 
         public void Attack()
         {
+            //if kirby is close, firebreath. else, fire projectile.
+            //for now, this is just fire breath
             stateMachine.ChangePose(EnemyPose.Attacking);
             UpdateTexture();
         }
 
         public void UpdateTexture()
         {
-             if(!stateMachine.GetStateString().Equals(oldState)){
+            if (!stateMachine.GetStateString().Equals(oldState))
+            {
                 enemySprite = SpriteFactory.Instance.createSprite(stateMachine.GetSpriteParameters());
-                 oldState = stateMachine.GetStateString();
-             } 
+                oldState = stateMachine.GetStateString();
+            }
         }
 
         public void Update(GameTime gameTime)
@@ -79,34 +84,46 @@ namespace MasterGame
             {
                 frameCounter++;
 
-                // Handle walking state
+                //walking
                 if (stateMachine.GetPose() == EnemyPose.Walking)
                 {
                     Move();
 
                     if (frameCounter >= walkFrames)
                     {
-                        stateMachine.ChangePose(EnemyPose.Charging); // Stop after walking to load attack
+                        //stateMachine.ChangePose(EnemyPose.Shooting);
+                        stateMachine.ChangePose(EnemyPose.Charging);
                         frameCounter = 0;
                         UpdateTexture();
                     }
                 }
-                // Handle idle (stopped) state
-                else if (stateMachine.GetPose() == EnemyPose.Charging) //If Loading Attack,
-                {
-                    if (frameCounter >= stopFrames)
+                //shooting
+               // else if (stateMachine.GetPose() == EnemyPose.Shooting)
+                else if (stateMachine.GetPose() == EnemyPose.Charging)
+                        {
+                    if (frameCounter == 1) // fireball projectile
                     {
-                        stateMachine.ChangePose(EnemyPose.Attacking); // Attack after stopping
+                        ShootProjectile();
+                    }
+
+                    if (frameCounter >= shootFrames)
+                    {
+                        stateMachine.ChangePose(EnemyPose.Attacking); // attacks (blows fire) after shooting
                         frameCounter = 0;
                         UpdateTexture();
                     }
                 }
-                // Handle attacking state
-                else if (stateMachine.GetPose() == EnemyPose.Attacking) //If attacking
+                //blows fire
+                else if (stateMachine.GetPose() == EnemyPose.Attacking)
                 {
+                    if (frameCounter == 1) //blowing fire projectile
+                    {
+                        Attack();
+                    }
+
                     if (frameCounter >= attackFrames)
                     {
-                        stateMachine.ChangePose(EnemyPose.Walking); // Walk again after attacking 
+                        stateMachine.ChangePose(EnemyPose.Walking); //walk after 
                         frameCounter = 0;
                         UpdateTexture();
                     }
@@ -114,6 +131,9 @@ namespace MasterGame
 
                 // Update sprite animation
                 enemySprite.Update();
+
+                // IF EXISTS update fireball
+                fireball?.Update();
             }
         }
 
@@ -140,11 +160,21 @@ namespace MasterGame
             }
         }
 
+        private void ShootProjectile()
+        {
+            if (fireball == null)
+            {
+                fireball = new EnemyFireball(position, new Vector2(1, -0.5f));
+            }
+        }
+
         public void Draw(SpriteBatch spriteBatch)
         {
             if (!isDead)
             {
                 enemySprite.Draw(position, spriteBatch);
+                // IF EXISTS, draw fireball
+                fireball?.Draw(spriteBatch);
             }
         }
 
