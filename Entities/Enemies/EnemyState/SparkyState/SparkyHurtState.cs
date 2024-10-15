c4da964a3b83e9cc3599552aeeb95eb859ca1a6e
gbarnholtz <dbcgwyn@gmail.com>
2024-10-15 02:24:29 -0400

    sparky state pattern

---
+++ Entities/Enemies/EnemyState/SparkyState/SparkyHurtState.cs
@@ -0,0 +1,31 @@
+﻿using KirbyNightmareInDreamLand.StateMachines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirbyNightmareInDreamLand.Entities.Enemies.EnemyState.SparkyState
+{
+    public class SparkyHurtState : IEnemyState
+    {
+        public void Enter(Enemy enemy)
+        {
+            enemy.StateMachine.ChangePose(EnemyPose.Hurt);
+            enemy.ResetFrameCounter();
+        }
+
+        public void Update(Enemy enemy)
+        {
+            // Logic for when Sparky is hurt
+
+            if (enemy.FrameCounter >= Constants.Sparky.HURT_FRAMES)
+            {
+                enemy.ChangeState(new SparkyShortJumpState());
+                enemy.UpdateTexture();
+            }
+        }
+
+        public void Exit(Enemy enemy) { }
+    }
+}
