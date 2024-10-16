92acf82bad5695eb012395954a2e74c5694f21f2
Payton Murphy <Pmackmurphy@outlook.com>
2024-10-15 22:37:49 -0400

    Refactored commands to take no parameters. Made a player list in Game1 instead of only one player

--- Commands/KirbyMoveRightCommand.cs
+++ Commands/KirbyMoveRightCommand.cs
@@ -6,17 +6,6 @@ namespace KirbyNightmareInDreamLand.Commands
 {
     public class KirbyMoveRightCommand : ICommand
     {
-        private KeyboardController keyboard;
-        private Keys key;
-        private IPlayer kirby;
-
-        public KirbyMoveRightCommand(IPlayer player, Keys keyMapped, KeyboardController currentKeyboard)
-        {
-            keyboard = currentKeyboard;
-            key = keyMapped;
-            kirby = player;
-        }
-
         public void Execute()
         {
             kirby.MoveRight();
