218706098add22217189804b48009579a1ac8074
gbarnholtz <dbcgwyn@gmail.com>
2024-10-16 17:32:28 -0400

    collision added not working tho

--- Entities/Enemies/Enemy.cs
+++ Entities/Enemies/Enemy.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace KirbyNightmareInDreamLand.Entities.Enemies
 {
-    public abstract class Enemy : IEnemy
+    public abstract class Enemy : IEnemy, ICollidable
     {
         protected Vector2 position; //Where enemy is drawn on screen
         protected int health; //Enemy health
@@ -148,7 +148,20 @@ namespace KirbyNightmareInDreamLand.Entities.Enemies
 
         public virtual void Fall() { }
 
-        public abstract void Move();      
+        public abstract void Move();
+
+        public Vector2 CalculateRectanglePoint(Vector2 pos)
+        {
+            float x = pos.X - Constants.HitBoxes.ENTITY_WIDTH / 2;
+            float y = pos.Y - Constants.HitBoxes.ENTITY_HEIGHT;
+            Vector2 rectPoint = new Vector2(x, y);
+            return rectPoint;
+        }
+        public Rectangle GetHitBox()
+        {
+            Vector2 rectPoint = CalculateRectanglePoint(position);
+            return new Rectangle((int)rectPoint.X, (int)rectPoint.Y, Constants.HitBoxes.ENTITY_WIDTH, Constants.HitBoxes.ENTITY_HEIGHT);
+        }
 
     }
 }
