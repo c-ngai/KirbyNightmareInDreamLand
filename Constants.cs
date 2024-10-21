6cd13038354523f6aff515acd332da817f6c63b4
Mark DeLeo <markdeleo02@gmail.com>
2024-10-20 22:01:12 -0400

    Small band-aid fixes to Enemy and WaddleDoo

--- Constants.cs
+++ Constants.cs
@@ -265,7 +265,7 @@ namespace KirbyNightmareInDreamLand
             public const int ATTACK_FRAMES = 50;
             public const int HURT_FRAMES = 50;
             public const int JUMPING_FRAMES = 50;
-            public const float JUMP_HEIGHT = 1.5f;
+            public const float JUMP_VELOCITY = 2.5f;
             public const float FORWARD_MOVEMENT = 0.5f;
             public const float DEAD_FRAMES = 20;
         }
