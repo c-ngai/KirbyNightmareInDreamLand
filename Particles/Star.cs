a72e200621da0115d5b31a2e525676bce285759c
Carman Ngai <carmanngai2003@gmail.com>
2024-10-31 00:44:43 -0400

    Setting up particles.

---
+++ Particles/Star.cs
@@ -0,0 +1,21 @@
+﻿//using Microsoft.Xna.Framework;
+//using Microsoft.Xna.Framework.Graphics;
+//using KirbyNightmareInDreamLand.Sprites;
+//using System;
+
+//namespace KirbyNightmareInDreamLand.Particles
+//{
+//    public class Star : IParticle
+//    {
+//        private Sprite sprite;
+//        private Vector2 position;
+//        private int frameCount = 0;
+//        private Random randomGenerator; 
+
+//        public Star()
+//        {
+//            randomGenerator = new Random();
+
+//        }
+//    }
+//}
