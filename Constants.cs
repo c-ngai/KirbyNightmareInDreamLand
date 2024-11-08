9daf6984cc47fa49b469e167b2c546bc4b93a2bc
goose0 <mare6213@gmail.com>
2024-11-07 22:51:53 -0500

    fixed treasure room death bug:

--- Constants.cs
+++ Constants.cs
@@ -145,7 +145,7 @@ namespace KirbyNightmareInDreamLand
 
         public class Kirby
         {
-            public const int MAX_HEALTH = 6;
+            public const int MAX_HEALTH = 1;
             public const int MAX_LIVES = 3;
             public static Vector2 BEAM_ATTACK_OFFSET_RIGHT = new Vector2(13, -7);
             public static Vector2 BEAM_ATTACK_OFFSET_LEFT = new Vector2(-13, -7);
