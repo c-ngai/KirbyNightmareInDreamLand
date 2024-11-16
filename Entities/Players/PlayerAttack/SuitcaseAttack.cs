116da856cf80c0837c310663e978e92d25ea62c2
goose0 <mare6213@gmail.com>
2024-11-16 18:01:52 -0500

    started suitcase attack

---
+++ Entities/Players/PlayerAttack/SuitcaseAttack.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Net;
+using KirbyNightmareInDreamLand.Audio;
+using KirbyNightmareInDreamLand.Entities.Players;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KirbyNightmareInDreamLand.Projectiles
+{
+    public class Suitcase : IProjectile, ICollidable
+    {
+        public bool CollisionActive { get; private set;} = true;
+        public Vector2 Position {get; private set;}
+        public Vector2 Velocity {get; private set;}
+        private float startingX;
+        private static int slideDistance = 70;
+        private bool IsLeft;
+        public Suitcase(Vector2 pos, bool isLeft)
+        {
+            IsLeft= isLeft;
+            Position =pos;
+            startingX = pos.X;
+            ObjectManager.Instance.RegisterDynamicObject(this);
+            SoundManager.Play("slide");
+        }
+        public void EndAttack()
+        {
+            CollisionActive = false;
+        }
+        public bool IsDone()
+        {
+            float distanceMoved = Math.Abs(Position.X - startingX);
+            if(distanceMoved > slideDistance || !CollisionActive)
+            {
+                EndAttack();
+                return true;
+            }
+            return false;
+        }
+        public string GetObjectType()
+        {
+            return Constants.CollisionObjectType.PLAYER_ATTACK;
+        }
+        public void Update()
+        {
+            GetHitBox();
+        }
+        public void Update(Player kirby)
+        {
+            Position = kirby.GetKirbyPosition();
+            GetHitBox();
+        }
+
+        public Vector2 CalculateRectanglePoint(Vector2 pos)
+        {
+            return pos + (IsLeft ? Constants.HitBoxes.SLIDE_OFFSET_LEFT: Constants.HitBoxes.SLIDE_OFFSET_RIGHT); 
+        }
+        public Rectangle GetHitBox()
+        {
+            Vector2 rectPoint = CalculateRectanglePoint(Position);
+            return new Rectangle((int)rectPoint.X, (int)rectPoint.Y, Constants.HitBoxes.SLIDE_WIDTH, Constants.HitBoxes.SLIDE_HEIGHT);
+        }
+        public Vector2 GetPosition()
+        {
+            return Position;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            //uneeded
+        }
+
+
+    }
+}
