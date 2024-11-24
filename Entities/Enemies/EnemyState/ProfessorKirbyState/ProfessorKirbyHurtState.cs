719c4b15da7a4b1d19024168553d88e71164d413
gbarnholtz <dbcgwyn@gmail.com>
2024-11-24 12:07:04 -0500

    added prof enemy class/states (same as waddle doo behavior, no sprites yet)

---
+++ Entities/Enemies/EnemyState/ProfessorKirbyState/ProfessorKirbyHurtState.cs
@@ -0,0 +1,59 @@
+﻿using KirbyNightmareInDreamLand.Entities.Enemies.EnemyState.WaddleDeeState;
+using KirbyNightmareInDreamLand.StateMachines;
+using System;
+
+namespace KirbyNightmareInDreamLand.Entities.Enemies.EnemyState.WaddleDooState
+{
+    public class ProfessorKirbyHurtState : IEnemyState
+    {
+        private readonly Enemy _enemy;
+
+        public ProfessorKirbyHurtState(Enemy enemy)
+        {
+            _enemy = enemy ?? throw new ArgumentNullException(nameof(enemy));
+        }
+
+        public void Enter()
+        {
+            _enemy.ChangePose(EnemyPose.Hurt);
+            _enemy.ResetFrameCounter();
+            _enemy.Health -= Constants.Enemies.DAMAGE_TAKEN; 
+        }
+
+        public void Update()
+        {
+
+            _enemy.IncrementFrameCounter();
+
+            //TO-DO: CHANGE TO WHEN KIRBY + ENEMY COLLIDE
+            if (_enemy.FrameCounter >= Constants.ProfessorKirby.HURT_FRAMES)
+            {
+                _enemy.ChangeState(new ProfessorKirbyWalkingState(_enemy));
+                _enemy.UpdateTexture();
+
+                if (_enemy.Health <= 0)
+                {
+                    _enemy.Active = false;
+                    _enemy.CollisionActive = false;
+                }
+            }
+        }
+
+        public void Exit() { }
+
+        public void TakeDamage()
+        {
+            //handled in update
+        }
+        public void ChangeDirection()
+        {
+            //won't change direction while hurt
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+    }
+}
