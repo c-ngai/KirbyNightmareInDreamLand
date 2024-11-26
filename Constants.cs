ac5a0ba3b177399ccf899355549aec862a272145
Mark DeLeo <markdeleo02@gmail.com>
2024-11-26 00:44:14 -0500

    Changed max lives and health constants back to their actual values

--- Constants.cs
+++ Constants.cs
@@ -176,7 +176,7 @@ namespace KirbyNightmareInDreamLand
         public class Kirby
         {
             public const float INVINCIBLE_TIME = 1.5f;
-            public const int MAX_HEALTH = 3; //6
+            public const int MAX_HEALTH = 6; //6
             public const int MAX_LIVES = 3; //3
             public const int STARTINGXPOSITION = 30;
             public static Vector2 BEAM_ATTACK_OFFSET_RIGHT = new Vector2(11, -9);
