f9a23dbe34135f1d83b233dc24879e2212b528e5
goose0 <mare6213@gmail.com>
2024-11-27 02:43:15 -0500

    code reviews

--- UI/HUD.cs
+++ UI/HUD.cs
@@ -202,7 +202,7 @@ namespace KirbyNightmareInDreamLand.UI
                     // Draw lives
                     hudElements["ui_lives"].Draw(Constants.HUD.LIVES_ICON_POS, spriteBatch);
 
-                    int displayLives = targetPlayer.lives; // Adjust to show 02 for 3 lives, 01 for 2 lives, etc.
+                    int displayLives = targetPlayer.lives;
                     string displayLivesText = displayLives.ToString().PadLeft(Constants.HUD.LIVES_PAD, '0'); // Format as two digits
 
                     int livesTens = int.Parse(displayLivesText[0].ToString());
