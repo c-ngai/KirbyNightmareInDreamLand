5422bf0acadb61b376afbb84d1080dab019f40f3
vivferrig <viv.ferrigni@gmail.com>
2024-11-28 12:19:16 -0600

    added new level to hub and spent an hour resolving comflicts fml

--- Game1.cs
+++ Game1.cs
@@ -174,7 +174,7 @@ namespace KirbyNightmareInDreamLand
 
             // Create level instance and load initial room
             Level = new Level();
-            Level.LoadRoom("level2_room1");
+            Level.LoadRoom("hub");
 
             // Load the desired keymap by name
             LevelLoader.Instance.LoadKeymap("main");
